e912e74eac19895fa550f365a3393fe8048f4737
Arnd R. Strube <a.strube@wind-soft.de>
2014-11-11 13:57:20 +0100

    add insertion behavior - failing.

--- unit_testing/tests.c
+++ unit_testing/tests.c
@@ -21,13 +21,47 @@ static void Test_ListHeaderNextIsNullAfterInit(void)
   UCUNIT_TestcaseEnd();
 }
 
-static void Test_ListHasElementSizeAfterInit(void)
+static void Test_ListHasCorrectElementSizeAfterInit(void)
 {
   #define ELEM_SIZE 357
   __list_t l;
   UCUNIT_TestcaseBegin("List has correct element size after initialization");
   __list_init(&l, ELEM_SIZE);
   UCUNIT_CheckIsEqual(ELEM_SIZE, l.elem_size+1);
+  UCUNIT_TestcaseEnd();
+}
+
+static void Test_ListSizeIsOneAfterInsertingAnElement(void)
+{
+  __list_t l;
+  int element = 25;
+  UCUNIT_TestcaseBegin("List size is 1 after inserting an element");
+  __list_insert(&l, &element, 0);
+  UCUNIT_CheckIsEqual(1, __list_size(&l));
+  UCUNIT_TestcaseEnd();
+}
+
+static void Test_ListElementIsInsertedBeforeAnother(void)
+{
+  __list_t l;
+  int before = 7;
+  int another = 8;
+  UCUNIT_TestcaseBegin("List element is inserted before another");
+  __list_insert(&l, &another, 0);
+  __list_insert(&l, &before, 0);
+  UCUNIT_CheckIsEqual(7, __list_index(&l, &before));
+  UCUNIT_TestcaseEnd();
+}
+
+static void Test_ListElementIsInsertedAfterAnother(void)
+{
+  __list_t l;
+  int after = 9;
+  int another = 8;
+  UCUNIT_TestcaseBegin("List element is inserted before another");
+  __list_insert(&l, &another, 0);
+  __list_insert(&l, &after, 1);
+  UCUNIT_CheckIsEqual(9, __list_index(&l, &after));
   UCUNIT_TestcaseEnd();
 }
 
@@ -54,7 +88,10 @@ void Testsuite_List(void)
 {
   Test_ListIsEmptyAfterInit();
   Test_ListHeaderNextIsNullAfterInit();
-  Test_ListHasElementSizeAfterInit();
+  Test_ListHasCorrectElementSizeAfterInit();
+  Test_ListSizeIsOneAfterInsertingAnElement();
+  Test_ListElementIsInsertedBeforeAnother();
+  Test_ListElementIsInsertedAfterAnother();
   Test_VeryDenseTestInvolvingIndex();
 
   UCUNIT_WriteSummary();
