79387d648f1783c34ed290bd91eb28e79128f7b4
Arnd R. Strube <a.strube@wind-soft.de>
2014-11-13 16:37:41 +0100

    Cspec - apply to tests step one

--- unit_testing/tests.c
+++ unit_testing/tests.c
@@ -3,13 +3,15 @@
 #include "__list.h"
 #include "tests.h"
 
+DESCRIBE(List, "List")
+
 static void Test_ListIsNotNullAfterCreation(void)
 {
-  UCUNIT_TestcaseBegin("List is not NULL after creation");
+  IT("List is not NULL after creation");
   __list_t* l = __list_create(sizeof(int));
   UCUNIT_CheckIsNotNull(l);
   __list_destroy(&l);
-  UCUNIT_TestcaseEnd();
+  IT_END;
 }
 
 static void Test_ListHeaderNextIsNullAfterCreation(void)
@@ -24,22 +26,22 @@ static void Test_ListHasCorrectElementSizeAfterInit(void)
 
 static void Test_ListSizeIsOneAfterInsertingAnElement(void)
 {
-  UCUNIT_TestcaseBegin("List size is 1 after inserting an element");
+  IT("List size is 1 after inserting an element");
   __list_t* l = __list_create(sizeof(int));
   int element = 25;
   __list_insert(l, &element, 0);
   UCUNIT_CheckIsEqual(1, __list_size(l)+1); // provoke failure
   __list_destroy(&l);
-  UCUNIT_TestcaseEnd();
+  IT_END;
 }
 
 static void Test_ListIsNullAfterDestruction(void)
 {
-  UCUNIT_TestcaseBegin("List is NULL after destruction");
+  IT("List is NULL after destruction");
   __list_t* l = __list_create(sizeof(int));
   __list_destroy(&l);
   UCUNIT_CheckIsNull(l);
-  UCUNIT_TestcaseEnd();
+  IT_END;
 }
 
 static void Test_ListIsEmptyAfterCleanup(void)
@@ -51,46 +53,46 @@ static void Test_ListElementIsInsertedBeforeAnother(void)
 {
   int before = 7;
   int another = 8;
-  UCUNIT_TestcaseBegin("List element is inserted before another");
+  IT("List element is inserted before another");
   __list_t* l = __list_create(sizeof(int));
   __list_insert(l, &another, 0);
   __list_insert(l, &before, 0);
   UCUNIT_CheckIsEqual(0, __list_index(l, &before));
   __list_destroy(&l);
-  UCUNIT_TestcaseEnd();
+  IT_END;
 }
 
 static void Test_ListElementIsInsertedAfterAnother(void)
 {
   int after = 9;
   int another = 8;
-  UCUNIT_TestcaseBegin("List element is inserted after another");
+  IT("List element is inserted after another");
   __list_t* l = __list_create(sizeof(int));
   __list_insert(l, &another, 0);
   __list_insert(l, &after, 1);
   UCUNIT_CheckIsEqual(1, __list_index(l, &after));
   __list_destroy(&l);
-  UCUNIT_TestcaseEnd();
+  IT_END;
 }
 static void Test_ListElementInsertedBeyondEndReturnsError(void)
 {
   int first = 3;
   int second = 0;
   int test = 15;
-  UCUNIT_TestcaseBegin("List element inserted beyond the end of the list results in error");
+  IT("List element inserted beyond the end of the list results in error");
   __list_t* l = __list_create(sizeof(int));
   __list_insert(l, &first, 0);
   __list_insert(l, &second, 1);
   UCUNIT_CheckIsEqual(-1, __list_insert(l, &test, 4));
   __list_destroy(&l);
-  UCUNIT_TestcaseEnd();
+  IT_END;
 }
 
 static void Test_ListIsEmptyAfterRemovingTheLastElement(void)
 {
   int input_elem = 5;
   int output_elem = -1;
-  UCUNIT_TestcaseBegin("List is empty after removing the last element");
+  IT("List is empty after removing the last element");
   __list_t* l = __list_create(sizeof(int));
   __list_insert(l, &input_elem, 0);
   __list_rem(l, &output_elem, 0);
@@ -98,80 +100,80 @@ static void Test_ListIsEmptyAfterRemovingTheLastElement(void)
   UCUNIT_CheckIsEqual(5, output_elem);
   UCUNIT_CheckIsEqual(0, __list_size(l));
   __list_destroy(&l);
-  UCUNIT_TestcaseEnd();
+  IT_END;
 }
 
 static void Test_ListReturnsErrorForInsertingAtMinusOne(void)
 {
   int element = 5;
-  UCUNIT_TestcaseBegin("List returns an error for inserting at index -1");
+  IT("List returns an error for inserting at index -1");
   __list_t* l = __list_create(sizeof(int));
   UCUNIT_CheckIsEqual(-1, __list_insert(l, &element, -1));
   __list_destroy(&l);
-  UCUNIT_TestcaseEnd();
+  IT_END;
 }
 
 static void Test_ListReturnsErrorForInsertingBeyondEnd(void)
 {
   int element = 5;
-  UCUNIT_TestcaseBegin("List returns an error for inserting beyond end");
+  IT("List returns an error for inserting beyond end");
   __list_t* l = __list_create(sizeof(int));
   UCUNIT_CheckIsEqual(-1, __list_insert(l, &element, 1));
   __list_destroy(&l);
-  UCUNIT_TestcaseEnd();
+  IT_END;
 }
 
 static void Test_ListCanGetFirstElement()
 {
   int element = 3;
-  UCUNIT_TestcaseBegin("List can get the first element");
+  IT("List can get the first element");
   __list_t* l = __list_create(sizeof(int));
   __list_insert(l, &element, 0);
   __list_get(l, &element, 0);
   UCUNIT_CheckIsEqual(3, element);
   __list_destroy(&l);
-  UCUNIT_TestcaseEnd();
+  IT_END;
 }
 
 static void Test_ListCanGetSecondElement()
 {
   int elements[] = {4, 3};
   int result;
-  UCUNIT_TestcaseBegin("List can get the second element");
+  IT("List can get the second element");
   __list_t* l = __list_create(sizeof(int));
   __list_insert(l, elements, 0);
   __list_insert(l, elements + 1, 1);
   __list_get(l, &result, 1);
   UCUNIT_CheckIsEqual(3, result);
   __list_destroy(&l);
-  UCUNIT_TestcaseEnd();
+  IT_END;
 }
 
 static void Test_ListReturnsErrorForTryingToGetAtIndexMinusOne()
 {
   int element = 5;
-  UCUNIT_TestcaseBegin("List returns an error for trying to get at -1");
+  IT("List returns an error for trying to get at -1");
   __list_t* l = __list_create(sizeof(int));
   UCUNIT_CheckIsEqual(-1, __list_get(l, &element, -1));
   __list_destroy(&l);
-  UCUNIT_TestcaseEnd();
+  IT_END;
 }
 
 static void Test_ListReturnsErrorForTryingToGetBeyondEnd()
 {
   int element = 5;
-  UCUNIT_TestcaseBegin("List returns an error for trying to get beyond end");
+  IT("List returns an error for trying to get beyond end");
   __list_t* l = __list_create(sizeof(int));
   UCUNIT_CheckIsEqual(-1, __list_get(l, &element, 1));
   __list_destroy(&l);
-  UCUNIT_TestcaseEnd();
+  IT_END;
 }
 
 static void Test_ListCanRemoveSecondElement()
 {
   int elements[] = {4, 3};
   int result;
-  UCUNIT_TestcaseBegin("List can remove the second element");
+  IT("List can remove the second element");
   __list_t* l = __list_create(sizeof(int));
   __list_insert(l, elements, 0);
   __list_insert(l, elements + 1, 1);
@@ -179,40 +181,42 @@ static void Test_ListCanRemoveSecondElement()
   UCUNIT_CheckIsEqual(3, result);
   UCUNIT_CheckIsEqual(1, __list_size(l));
   __list_destroy(&l);
-  UCUNIT_TestcaseEnd();
+  IT_END;
 }
 
 static void Test_ListReturnsErrorForTryingToRemoveAtMinusOne()
 {
   int element = 5;
-  UCUNIT_TestcaseBegin("List returns an error for trying to remove at -1");
+  IT("List returns an error for trying to remove at -1");
   __list_t* l = __list_create(sizeof(int));
   UCUNIT_CheckIsEqual(-1, __list_rem(l, &element, -1));
   __list_destroy(&l);
-  UCUNIT_TestcaseEnd();
+  IT_END;
 }
 
 static void Test_ListReturnsErrorForTryingToRemoveBeyondEnd()
 {
   int element = 5;
-  UCUNIT_TestcaseBegin("List returns an error for trying to remove beyond end");
+  IT("List returns an error for trying to remove beyond end");
   __list_t* l = __list_create(sizeof(int));
   UCUNIT_CheckIsEqual(-1, __list_rem(l, &element, 1));
   __list_destroy(&l);
-  UCUNIT_TestcaseEnd();
+  IT_END;
 }
 
 static Test_ListCanFindAnElement(void)
 {
   int elements[] = {4, 5};
-  UCUNIT_TestcaseBegin("List can find an element");
+  IT("List can find an element");
   __list_t* l = __list_create(sizeof(int));
   __list_insert(l, elements, 0);
   __list_insert(l, elements+1, 1);
   UCUNIT_CheckIsEqual(1, __list_index(l, &elements[1]));
   __list_destroy(&l);
-  UCUNIT_TestcaseEnd();
+  IT_END;
 }
+
+DESCRIBE_END
 
 void Testsuite_List(void)
 {
